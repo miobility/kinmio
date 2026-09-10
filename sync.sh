807964f4ba9ad53d8089098cb4fb82caa2b83fb6
Benjamin RAJJOU <benja@MacBook-Air.local>
2026-09-10 18:55:37 +0200

    Sync 2026-09-10 18:55

--- sync.sh
+++ sync.sh
@@ -5,7 +5,7 @@ set -e
 
 SRC="/Users/benja/Desktop/APP KINMIO"
 REPO="$SRC/kinmio-repo"
-FILES="index.html kinmio_logo_app.png kinmio_logo_app_light.png kinmio_n.png kinmio_n_light.png kinmio_logo.jpeg Marc.jpg Sophie.jpg female1.jpg male1.jpg shadow.jpg shadow.mp4 onboarding.mp4 onboarding2.mp4 ob_vision.mp4 ob_demo_step1.mp4 ob_demo_step2.mp4 ob_demo_step3.mp4 programme-cardio.jpg programme-entretien.jpg programme-mobilite.jpg programme-renforcement.jpg exercise-01.jpg exercise-02.jpg exercise-03.jpg exercise-04.jpg exercise-05.jpg exercise-06.jpg exercise-07.jpg exercise-08.jpg exercise-09.jpg exercise-10.jpg exercise-11.jpg exercise-12.jpg exercise-13.jpg exercise-14.jpg exercise-15.jpg exercise-16.jpg exercise-17.jpg exercise-18.jpg exercise-19.jpg exercise-20.jpg"
+FILES="index.html kinmio_logo_app.png kinmio_logo_app_light.png kinmio_n.png kinmio_n_light.png kinmio_logo.jpeg Marc.jpg Sophie.jpg female1.jpg male1.jpg benjamin.webp shadow.jpg shadow.mp4 onboarding.mp4 onboarding2.mp4 ob_vision.mp4 ob_demo_step1.mp4 ob_demo_step2.mp4 ob_demo_step3.mp4 programme-cardio.jpg programme-entretien.jpg programme-mobilite.jpg programme-renforcement.jpg exercise-01.jpg exercise-02.jpg exercise-03.jpg exercise-04.jpg exercise-05.jpg exercise-06.jpg exercise-07.jpg exercise-08.jpg exercise-09.jpg exercise-10.jpg exercise-11.jpg exercise-12.jpg exercise-13.jpg exercise-14.jpg exercise-15.jpg exercise-16.jpg exercise-17.jpg exercise-18.jpg exercise-19.jpg exercise-20.jpg"
 
 cd "$REPO"
 
